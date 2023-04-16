e589fae5a8390e937239a75b1daad9887c04be73
lucasfacci <lucasfacci@protonmail.com>
2023-04-16 05:18:16 -0300

    improving create user page and adding users route

--- src/forms.py
+++ src/forms.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 
-class SimpleForm(FlaskForm):
+class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={'placeholder': 'Usuário'})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=80)], render_kw={'placeholder': 'Senha'})
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={'placeholder': 'Usuário'})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=80)], render_kw={'placeholder': 'Senha'})
+    password_confirm = PasswordField(validators=[InputRequired(), Length(min=4, max=80)], render_kw={'placeholder': 'Confirme a Senha'})
--- src/views.py
+++ src/views.py
@@ -1,24 +1,24 @@
-from flask import render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import app, db
-from .forms import SimpleForm
+from .forms import LoginForm, RegisterForm
 from .models import User
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     logout_user()
-    form = SimpleForm()
+    form = LoginForm()
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
-            login_user(user)
-            return redirect(url_for('index'))
-        else:
-            error_message = 'Usuário e/ou senha inválidos(as)!'
-            return render_template("login.html",form=form, message=error_message)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                return render_template("login.html", form=form, message='Usuário e/ou senha inválidos(as)!')
                 
     return render_template('login.html', form=form)
 
@@ -26,14 +26,16 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 @login_required
 def register():
-    form = SimpleForm()
+    form = RegisterForm()
 
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
-        db.session.add(User(username=form.username.data, password=hashed_password))
-        db.session.commit()
-        message = 'Usuário criado com sucesso!'
-        return render_template('register.html', message=message)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.password.data == form.password_confirm.data:
+                db.session.add(User(username=form.username.data, password=generate_password_hash(form.password.data)))
+                db.session.commit()
+                return render_template('register.html', form=form, success_message='Usuário criado com sucesso!')
+            else:
+                return render_template("register.html", form=form, error_message='As senhas não correspondem!')
     
     return render_template('register.html', form=form)
 
@@ -49,3 +51,9 @@ def logout():
 @login_required
 def index():
     return render_template('index.html')
+
+
+@app.route('/users')
+@login_required
+def users():
+    return render_template('users.html')
